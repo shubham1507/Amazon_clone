f65cc10b19af186b02f610ed2bc8291f521f3b01
shubham1507 <shubham.joshi1507@gmail.com>
2021-04-02 10:06:33 +0530

    Added Database Models Receiver Code

--- An_app/models.py
+++ An_app/models.py
@@ -180,6 +180,8 @@
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 import uuid
 
 # Create your models here.
@@ -363,3 +365,35 @@ class OrderDeliveryStatus(models.Model):
     status = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+
+        print("created")
+
+        print(instance)
+
+        if instance.user_type == 1:
+            AdminUser.objects.create(auth_user_id=instance)
+        if instance.user_type == 2:
+            StaffUser.objects.create(auth_user_id=instance)
+        if instance.user_type == 3:
+            MerchantUser.objects.create(
+                auth_user_id=instance, company_name="", gst_details="", address="")
+        if instance.user_type == 4:
+            CustomerUser.objects.create(auth_user_id=instance)
+
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+
+    if instance.user_type == 1:
+        instance.adminuser.save()
+    if instance.user_type == 2:
+        instance.staffuser.save()
+    if instance.user_type == 3:
+        instance.merchantuser.save()
+    if instance.user_type == 4:
+        instance.customeruser.save()
